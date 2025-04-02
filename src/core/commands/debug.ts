0a5633b7ad1c9b012fe42c9ba307a2b329801b71
Dvop <75065860+DvopHeh@users.noreply.github.com>
2025-04-02 15:08:03 +0200

    Update debug.ts

--- src/core/commands/debug.ts
+++ src/core/commands/debug.ts
@@ -16,7 +16,7 @@ export default () => <ApplicationCommand>{
     execute([ephemeral], ctx) {
         const info = getDebugInfo();
         const content = [
-            "***Cumcord Debug Info***",
+            "***💦Cumcord Debug Info💦***",
             `> Cumcord: ${info.bunny.version} (${info.bunny.loader.name} ${info.bunny.loader.version})`,
             `> Discord: ${info.discord.version} (${info.discord.build})`,
             `> React: ${info.react.version} (RN ${info.react.nativeVersion})`,
